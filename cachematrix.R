f2acebb514a23e61067c35b52b508a55b61077c4
Jie Yong <jyong@umd.edu>
2015-07-23 10:45:38 -0400

    edited

--- cachematrix.R
+++ cachematrix.R
@@ -9,7 +9,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setinverse <- function(solve) m <<- mean
+  setinverse <- function(solve) m <<- solve
   getinverse <- function() m
   list(set = set, get = get,
        setinverse= setinverse,
